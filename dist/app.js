2886f2be0fd3c2efbdcb18d127d1a0867412e10f
Brian <brianhammer777@yahoo.com>
2024-09-15 15:18:44 -0400

    first commit

---
+++ dist/app.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var Engine_1 = require("./Engine");
+window.onload = function () {
+    var e = new Engine_1.Engine();
+    e.start();
+    console.log("starting");
+};
+//# sourceMappingURL=app.js.map
---
+++ src/app.ts
@@ -0,0 +1,7 @@
+import * as TSE from "./Engine";
+
+window.onload = function () {
+  let e = new TSE.Engine();
+  e.start();
+  console.log("starting");
+};
